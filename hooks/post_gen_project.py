fd6f25109d65c5614eabb6d2df2aeed6af8ae4d4
Wallace Reis <wallace@reis.me>
2019-08-27 15:56:57 +0000

    Move into src/ subdir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         remove_file('tests/__init__.py')
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
+        cli_file = os.path.join('src', '{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
